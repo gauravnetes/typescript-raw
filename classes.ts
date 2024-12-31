51641f85a053f92102768f1ead93ccfbc2fdde23
gauravnetes <gouravchandra935@gmail.com>
2024-12-31 23:18:28 +0530

    initial commit

---
+++ classes.ts
@@ -0,0 +1,17 @@
+interface Quiz {
+    name: string, 
+    type: string
+}
+
+interface Course {
+    name: string, 
+    author: string,     
+    subject: string
+}
+
+class Sellable<T> {
+    public cart: T[] = []
+    addToCart(product: T) {
+        this.cart.push(product)
+    }
+}
