f9b8d157e3098d0d7a948e967a05419c3369d455
gauravnetes <gouravchandra935@gmail.com>
2025-01-04 00:59:44 +0530

    added

---
+++ functions.ts
@@ -0,0 +1,22 @@
+function addTwo (num: number) {
+    // num.toUpperCase()  now we can't perform string or any other datatypes operations on this number
+    return num + 2; 
+}
+
+function getUpper(val: string) {
+    return val.toUpperCase()
+}
+
+function signUpUser(name: string, email: string, isPaid: boolean) {
+    return 
+}
+
+let loginUser = (name: string , email: string, isPaid: boolean = false) => {
+
+}
+addTwo(5)
+getUpper("two")
+signUpUser("gourav", "gou@gmail.com", true)
+loginUser("h", "gou@gmail.com")
+
+export {}
